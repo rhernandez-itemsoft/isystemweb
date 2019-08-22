795a7ac8ca892b7da68fc11feea549ce5100c6cc
Ricardo Hernández <rherl23@gmail.com>
2019-08-22 18:15:05 -0500

    Agregamos dashboard y sbdadmin, ya funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,95 +1,14 @@
-import { Component } from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  items: MenuItem[];
+export class AppComponent implements OnInit {
+    constructor() {
+    }
 
-
-  value: Date;
-
-  title = 'isystemweb';
-
-  ngOnInit() {
-    this.items = [
-        {
-            label: 'File',
-            icon: 'pi pi-fw pi-file',
-            items: [{
-                    label: 'New', 
-                    icon: 'pi pi-fw pi-plus',
-                    items: [
-                        {label: 'Project'},
-                        {label: 'Other'},
-                    ]
-                },
-                {label: 'Open'},
-                {separator:true},
-                {label: 'Quit'}
-            ]
-        },
-        {
-            label: 'Edit',
-            icon: 'pi pi-fw pi-pencil',
-            items: [
-                {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-            ]
-        },
-        {
-            label: 'Help',
-            icon: 'pi pi-fw pi-question',
-            items: [
-                {
-                    label: 'Contents'
-                },
-                {
-                    label: 'Search', 
-                    icon: 'pi pi-fw pi-search', 
-                    items: [
-                        {
-                            label: 'Text', 
-                            items: [
-                                {
-                                    label: 'Workspace'
-                                }
-                            ]
-                        },
-                        {
-                            label: 'File'
-                        }
-                ]}
-            ]
-        },
-        {
-            label: 'Actions',
-            icon: 'pi pi-fw pi-cog',
-            items: [
-                {
-                    label: 'Edit',
-                    icon: 'pi pi-fw pi-pencil',
-                    items: [
-                        {label: 'Save', icon: 'pi pi-fw pi-save'},
-                        {label: 'Update', icon: 'pi pi-fw pi-save'},
-                    ]
-                },
-                {
-                    label: 'Other',
-                    icon: 'pi pi-fw pi-tags',
-                    items: [
-                        {label: 'Delete', icon: 'pi pi-fw pi-minus'}
-                    ]
-                }
-            ]
-        },
-        {separator:true},
-        {
-            label: 'Quit', icon: 'pi pi-fw pi-times'
-        }
-    ];
-}
+    ngOnInit() {
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LanguageTranslationModule } from './shared/modules/language-translation/language-translation.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {CommonModule} from '@angular/common';
-import {FormsModule}    from '@angular/forms'
-import {CalendarModule} from 'primeng/calendar';
-import {MenubarModule} from 'primeng/menubar';
-
+import { AuthGuard } from './shared';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-
-    CommonModule,
-    FormsModule,
-    CalendarModule,
-    MenubarModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        CommonModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        LanguageTranslationModule,
+        AppRoutingModule
+    ],
+    declarations: [AppComponent],
+    providers: [AuthGuard],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
